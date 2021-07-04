872538346e50e7e32899baa7a35c844dcbe384fa
Patrick Bedkowski <72998698+patrick-bedkowski@users.noreply.github.com>
2021-07-04 15:59:35 +0200

    Updated solution for question 2

---
+++ README.md
@@ -0,0 +1,7 @@
+# Recruitment tasks
+
+- Which brewery produces the strongest beers by abv ?
+- If you had to pick 3 beers to recommend to someone, how would you approach the problem ?
+
+## Solution
+<img src="exercise_2.png" alt="exercise_2"/>
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-'''
-And for ONE selected question out of the followings:
-- If you had to pick 3 beers to recommend to someone, how would you approach the problem?
-- What are the factors that impacts the quality of beer the most?
-- I enjoy a beer which aroma and appearance matches the beer style. What beer should I buy?
-'''
 import pandas as pd
 import numpy as np
+from tabulate import tabulate
 
 # get the dataset and create a data frame
 df = pd.read_csv('https://query.data.world/s/epkvquv6dgo5zi337wa2n23div4i3w')
@@ -21,46 +16,53 @@ max_abv_value = max(df["beer_abv"])
 max_abv_beers_indecies = [list(df["beer_abv"]).index(max_abv_value) for beer_abv in list(df["beer_abv"]) if beer_abv == max_abv_value]
 max_abv_beer_index = list(df["beer_abv"]).index(max_abv_value)
 
-if len(max_abv_beers_indecies) > 1:
+if len(max_abv_beers_indecies) < 2:
     # print the name of the brewery that produces beer with highest abv
-    print(f'Beer with maximum abv is produced by brewery: {df["brewery_name"][max_abv_beer_index]}')
+    print(f'Beer with maximum abv is produced by brewery: \n\t> {df["brewery_name"][max_abv_beer_index]}')
 else:
     print('Beer with maximum abv are produced by breweries:')
     for index in max_abv_beers_indecies:
-        print(df["brewery_name"][index])
+        print(f'\t> {df["brewery_name"][index]}')
 
 # =================================================== #
 # If you had to pick 3 beers to recommend to someone, #
 # how would you approach the problem?                 #
 # =================================================== #
 
+'''
+To answer this question, I will break down the solution into the following parts:
+    1. Create a data stating how many times each beer have received maximum score
+    2. Find 3 beers which have the most number of maximum overall reviews
+    and are of different beer style
+SQL query: SELECT DISTINCT beer_style FROM beer_reviews WHERE beer_name=""
+'''
+
 max_score = max(df['review_overall'])  # max score achieved by any beer
-number_of_rows = len(list(df['review_overall']))  # number of rows from dataframe
+n_of_indecies = len(list(df['review_overall']))  # number of rows from dataframe
 
-'''Find which beers achieved maximum overall review'''
+n_of_beer_reviews = {}  # dict that holds how many times specific beer has received max score reviews and its style
 
-# best_beers = []
-
-# for index in range(number_of_rows):
-#     if df['review_overall'][index] == max_score:
-#         if df['beer_name'][index] not in best_beers:
-#             best_beers.append(df['beer_name'][index])
-
-# print(best_beers)
-
-'''Find 3 beers which have the most number of maximum overall reviews'''
-
-n_of_beer_reviews = {}  # dict that holds beer names and number of 5.0 reviews
-
-for index in range(number_of_rows):
-    if df['review_overall'][index] == max_score:
+for index in range(n_of_indecies):  # iterate through indecies
+    if df['review_overall'][index] == max_score:  # if review is max score
         beer_name = df['beer_name'][index]
-        if beer_name not in n_of_beer_reviews.keys():
-            n_of_beer_reviews[beer_name] = 1
+        beer_style = df['beer_style'][index]
+        if beer_name not in n_of_beer_reviews.keys():  # if beer name not in dictionary
+            n_of_beer_reviews[beer_name] = [1, beer_style]
         else:
-            n_of_beer_reviews[beer_name] += 1
+            n_of_beer_reviews[beer_name][0] += 1  # increase number of max score reviews
 
-# sort a dictionary by value
-sorted = {k: v for k, v in sorted(n_of_beer_reviews.items(), key=lambda item: item[1])}
+# sort a dictionary by descending value using lambda function
+n_of_beer_reviews = {k: v for k, v in sorted(n_of_beer_reviews.items(), key=lambda item: item[1], reverse=True)}
 
-print(list(sorted.items())[-3:])  # print 3 last elements from dict converted to a list
+best_beers = [] # best beers data
+for beer_name, [beer_score, beer_style] in n_of_beer_reviews.items():  # iterate through dictionary
+    if len(best_beers) == 3:
+        break
+    else:
+        beer_styles = (beer_data[2] for beer_data in best_beers)
+        if beer_style in beer_styles: # if there is a beer with the same style
+            continue
+        else:
+            best_beers.append([beer_name, beer_score, beer_style])
+
+print(tabulate(best_beers, tablefmt="fancy_grid", headers=["Rank", "Beer name", "Number of best reviews", 'Beer style'], showindex=[1,2,3]))
